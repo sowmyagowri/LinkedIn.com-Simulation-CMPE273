a1a7a1929aec37615a00736d72b7b4aff9d6b6d7
Nirbhay Kekre <Nirbhay.Kekre@gmail.com>
2018-11-10 23:00:35 -0800

    adding cors

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,8 @@ let signupRecruiter = require('./routes/signupRecruiter');
 let signinRecruiter = require('./routes/signinRecruiter');
 let expressValidator = require("express-validator");
 var morgan = require('morgan');
+let cors = require('cors');
+const config = require('./config');
 const app = express()
 app.use(cookieParser());
 const router = express.Router()
@@ -22,8 +24,18 @@ let port = 5000 || process.env.PORT
 
 /** set up middlewares */
 
+app.use(cors({ origin: `http://${config.frontend_host}`, credentials: true }));
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', `http://${config.frontend_host}`);
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+    res.setHeader('Cache-Control', 'no-cache');
+    next();
+  });
+
 app.use(expressValidator());
 app.use(morgan('dev'));
 
