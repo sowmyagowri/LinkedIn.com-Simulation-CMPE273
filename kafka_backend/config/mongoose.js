f87a82599d7029da39523cd43a04c910e3173509
Sowmya Gowrishankar <gsowmya@gmail.com>
2018-11-23 22:52:13 -0800

    Update useNewUrlParser option in mongoose connection

--- kafka_backend/config/mongoose.js
+++ kafka_backend/config/mongoose.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://nrupa16:16Jan91*@ds159263.mlab.com:59263/linkedin_project', { poolSize: 10 }).then(
+mongoose.connect('mongodb://nrupa16:16Jan91*@ds159263.mlab.com:59263/linkedin_project', { useNewUrlParser: true, poolSize: 10 } ).then(
     () => {
         console.log("Getting MongoDB Connection!!!")
     },
