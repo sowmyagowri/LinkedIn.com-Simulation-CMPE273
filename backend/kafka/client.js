402fb9812f3cf58d7016569b39d8014ab767f415
Nirbhay Kekre <Nirbhay.Kekre@gmail.com>
2018-11-26 18:28:32 -0800

    enabling kafka

--- backend/kafka/client.js
+++ backend/kafka/client.js
@@ -1,6 +1,6 @@
 var rpc = require('./kafkarpc');
 const rpcConnections ={}
-const {byPassKafka} = require("./../config/adapter");
+
 /**
  * make request to kafka
  * 
@@ -10,13 +10,14 @@ const {byPassKafka} = require("./../config/adapter");
  * @param {function} callback - callback funtion to be executed upon success or failure of the kafka request
  */
 function make_request(req_queue_name, resp_queue_name, msg_payload, callback) {
-	/*if(rpcConnections[req_queue_name+"_"+resp_queue_name] === undefined){
+	if(rpcConnections[req_queue_name+"_"+resp_queue_name] === undefined){
 		// making sure only one instance per request response topic combination exist.
 		rpcConnections[req_queue_name+"_"+resp_queue_name] = new rpc() ;
 	}
 	console.log('in make request');
 	console.log(msg_payload);
 	rpcConnections[req_queue_name+"_"+resp_queue_name].makeRequest(req_queue_name, resp_queue_name, msg_payload, function (err, response) {
+
 		if (err) {
 			console.error(err);
 			callback(err, null);
@@ -24,8 +25,7 @@ function make_request(req_queue_name, resp_queue_name, msg_payload, callback) {
 			console.log("response", response);
 			callback(null, response);
 		}
-	});*/
-	byPassKafka(req_queue_name, msg_payload, callback);
+	});
 }
 
 exports.make_request = make_request;
