29b540d7dcd2b0a91555b5e09958a6826dad0e9c
Yash <yashmahajan19@gmail.com>
2018-11-29 20:41:30 -0800

    searchPeople API

--- backend/server.js
+++ backend/server.js
@@ -36,10 +36,9 @@ let getAllConnections = require('./routes/getAllConnections');
 let applyForJob  = require('./routes/applyForJob');
 let saveJob = require('./routes/saveJob');
 let getAllSavedJobs = require('./routes/getAllSavedJobs');
-
 let deleteProfile = require('./routes/deleteProfile');
-
 let searchJobs = require('./routes/searchJobs');
+let searchPeople = require('./routes/searchPeople');
 
 let expressValidator = require("express-validator");
 var morgan = require('morgan');
@@ -112,10 +111,9 @@ app.use("/getConnections", getAllConnections);
 app.use("/apply_for_job/", applyForJob);
 app.use("/save_job/", saveJob);
 app.use("/get_all_saved_jobs/", getAllSavedJobs);
-
 app.use("/delete_profile/", deleteProfile);
-
 app.use("/searchJobs", searchJobs);
+app.use("/searchPeople", searchPeople);
 
 
 /** start server */
