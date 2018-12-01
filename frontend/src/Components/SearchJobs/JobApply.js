a4714ddff878e6737a3e2ef6a39919e6f7d881a4
Saranya <saranya.vsrajan@gmail.com>
2018-11-30 20:08:15 -0800

    Update JobApply.js

--- frontend/src/Components/SearchJobs/JobApply.js
+++ frontend/src/Components/SearchJobs/JobApply.js
@@ -10,6 +10,7 @@ import URI from '../../constants/URI';
 import { userConstants } from '../../constants';
 import { getapplicantprofile } from '../../Actions/applicant_login_profile_actions';
 import { applyjob } from '../../Actions/actions_jobs';
+import validator from 'validator';
 
 
 class JobApply extends Component{
@@ -22,11 +23,14 @@ class JobApply extends Component{
           lastname : "",
           phonenumber : "",
           email : "",
+          address : "",
           profilephoto : "",
           resume : "",
-          Coverletter : "",
+          coverletter : "",
           ethnicity : "",
           question : "",
+          uploadedresume : "",
+          uploadcoverletter : "",
           sponsorship : false,
           disability : false,
           touchedprofile : {
@@ -35,8 +39,19 @@ class JobApply extends Component{
             phonenumber : false,
             email : false,
           },
-          uploadedresume: "",
-          uploadedcoverletter: ""
+          touchedfields : {
+            firstname: false,
+            lastname: false,
+            phonenumber : false,
+            email : false,
+            ethnicity : false,
+            address : false,
+            question : false,
+            resume : false,
+            coverletter : false,
+            sponsorship : false,
+            disability : false,
+          },
         };
 
         this.submitApply = this.submitApply.bind(this);
@@ -44,7 +59,7 @@ class JobApply extends Component{
         this.uploadresume = this.uploadresume.bind(this);
         this.uploadcoverletter = this.uploadcoverletter.bind(this);
         this.openResumeDialog = this.openResumeDialog.bind(this);
-        this.openCoverLetterDialog = this.openCoverLetterDialog.bind(this);
+        this.openCoverletterDialog = this.openCoverletterDialog.bind(this);
     }
 
 
@@ -79,7 +94,8 @@ class JobApply extends Component{
         document.getElementById('resume').click();
     }
 
-    openCoverLetterDialog = (e) => {
+
+    openCoverletterDialog = (e) => {
         document.getElementById('coverletter').click();
     }
 
@@ -100,7 +116,7 @@ class JobApply extends Component{
         console.log(file)
 
         this.setState ({
-            Coverletter : file.name,
+            coverletter : file.name,
             uploadedcoverletter : file
         })
     }
@@ -116,14 +132,21 @@ class JobApply extends Component{
 
     handleBlur = (field) => (evt) => {
         this.setState({
-            touchedprofile: { ...this.state.touchedprofile, [field]: true }
+            touchedprofile: { ...this.state.touchedprofile, [field]: true },
+            touchedfields: { ...this.state.touchedfields, [field]: true }
+        });
+    }
+
+    halffilled = (field) => (evt) => {
+        this.setState({
+            touchedfields: { ...this.state.touchedfields, [field]: true }
         });
     }
 
     handleValidation () {
         let formIsValid = false;
-        const errors = validateprofile(this.state.firstname, this.state.lastname, this.state.phonenumber, this.state.email, this.state.resume);
-        if(!errors.firstname && !errors.lastname && !errors.lastname && !errors.phonenumber && !errors.email && !errors.resume){
+        const errors = validateprofile(this.state.firstname, this.state.lastname, this.state.phonenumber, this.state.email, this.state.address, this.state.resume);
+        if(!errors.firstname && !errors.lastname && !errors.lastname && !errors.phonenumber && !errors.email && !errors.address && !errors.resume){
           formIsValid = true
         }
         return formIsValid;
@@ -148,7 +171,7 @@ class JobApply extends Component{
             var formData = new FormData();
             formData.append('uploadedResume', this.state.uploadedresume);
             if (this.state.Coverletter) {
-                formData.append('uploadedCoverletter', this.state.uploadedcoverletter);
+                formData.append('uploadedcoverletter', this.state.uploadedcoverletter);
             }
             
             Object.keys(data).forEach(function(key){
@@ -174,7 +197,7 @@ class JobApply extends Component{
         const {profile, jobdetails} = this.state;
         const {isLoading} = this.state;
         if(!isLoading){
-            const errors = validateprofile(this.state.firstname, this.state.lastname, this.state.phonenumber, this.state.email, this.state.resume);
+            const errors = validateprofile(this.state.firstname, this.state.lastname, this.state.phonenumber, this.state.email, this.state.address, this.state.resume);
             var shouldMarkError = (field) => {
                 const hasError = errors[field];
                 const shouldShow = this.state.touchedprofile[field];
@@ -249,6 +272,17 @@ class JobApply extends Component{
                                     }
                                 </div>
                                 <div className="row form-group">
+                                   <div className = "col-xs-12 col-md-12">
+                                        <label htmlFor="position-address-typeahead" className="mb1 required">Address*</label>
+                                        <input className = "form-control" name = "address" onChange = {this.changeHandler} onBlur={this.handleBlur('address')} id="position-address-typeahead" maxLength="100" type="text"/>
+                                    </div>
+                                    {!isLoading ?
+                                        <div className = "col-xs-12 col-md-12">
+                                        {shouldMarkError('address') ? <div className=""  style = {{color: "red"}}>Address is a required field</div> : (null)}
+                                        </div> : (null)
+                                    }
+                                </div>
+                                <div className="row form-group">
                                     <div className = "col-xs-6 col-md-6">
                                         <label htmlFor = "phone-number-question" className = "question-apply">Phone Number*</label>
                                         <input className = "form-control" name = "phonenumber" id="phone-number-question"  onChange = {this.changeHandler} type="text" pattern="[0-9]{10}" onBlur={this.handleBlur('phonenumber')} placeholder="1234567890"/>
@@ -271,11 +305,11 @@ class JobApply extends Component{
                                     <div className="row form-group">
                                         <div className = "col-xs-6 col-md-6">
                                             <label htmlFor = "ethnicity-question" className = "question-apply">Ethnicity</label>
-                                            <input className = "form-control" name = "ethnicity" id="ethnicity-question"  onChange = {this.changeHandler} type="text" placeholder=""/>
+                                            <input className = "form-control" name = "ethnicity" id="ethnicity-question"  onBlur={this.halffilled('ethnicity')} onChange = {this.changeHandler} type="text" placeholder=""/>
                                         </div>
                                         <div className = "col-xs-6 col-md-6">
                                             <label htmlFor = "question-question" className = "question-apply">How did you hear about us?</label>
-                                            <input className = "form-control" name = "question" id="question-question"  onChange = {this.changeHandler} type="text" placeholder=""/>
+                                            <input className = "form-control" name = "question" id="question-question" onBlur={this.halffilled('question')} onChange = {this.changeHandler} type="text" placeholder=""/>
                                         </div>
                                     </div>
                             </li>
@@ -284,18 +318,18 @@ class JobApply extends Component{
                             <div className = "question-apply">Will you now, or in the future, require sponsorship for employment visa status (e.g. H-1B visa status)? *</div>
                             <div className="radio">
                                 <h4><label>
-                                    <input type="radio" value = "True" name="sponsorship" onChange={this.changeHandler} />&nbsp;&nbsp;Yes</label> </h4>
+                                    <input type="radio" value = "True" name="sponsorship" onBlur={this.halffilled('sponsorship')} onChange={this.changeHandler} />&nbsp;&nbsp;Yes</label> </h4>
                                 <h4><label>
-                                    <input type="radio" value = "False" checked={this.state.sponsorship === "False"} name="sponsorship" onChange={this.changeHandler} />&nbsp;&nbsp;No</label> </h4>
+                                    <input type="radio" value = "False" checked={this.state.sponsorship === "False"} onBlur={this.halffilled('sponsorship')} name="sponsorship" onChange={this.changeHandler} />&nbsp;&nbsp;No</label> </h4>
                                 <br />
                             </div>
 
                             <div className = "question-apply">Do you have a physical, sensory, or mental condition that substantially limits any of your major life functions?*</div>
                             <div className="radio">
                                 <h4><label>
-                                    <input type="radio" value = "True" name="disability" onChange={this.changeHandler} />&nbsp;&nbsp;Yes</label> </h4>
+                                    <input type="radio" value = "True" name="disability"  onBlur={this.halffilled('disability')}  onChange={this.changeHandler} />&nbsp;&nbsp;Yes</label> </h4>
                                 <h4><label>
-                                    <input type="radio" value = "False" checked={this.state.disability === "False"} name="disability" onChange={this.changeHandler} />&nbsp;&nbsp;No</label> </h4>
+                                    <input type="radio" value = "False" checked={this.state.disability === "False"} onBlur={this.halffilled('disability')}  name="disability" onChange={this.changeHandler} />&nbsp;&nbsp;No</label> </h4>
                                 <br />
                             </div>
 
@@ -303,15 +337,15 @@ class JobApply extends Component{
                         <section className = "section-profile ember-view">
                         <div className = "profile-title" style = {{fontSize : "19px"}}>Resume</div>
                             <div className="form-group">
-                                <input type="file" id="resume" onChange={this.uploadresume} style = {{display : "none"}}/>
+                                <input type="file" id="resume" onChange={this.uploadresume} onBlur={this.halffilled('resume')} style = {{display : "none"}}/>
                                 <button type="file" className="btn arteco-btn-save" id="position-resume-typeahead" onClick = {this.openResumeDialog} style = {{width : "150px"}}>Upload Resume
                                 </button>&nbsp;&nbsp;{this.state.resume} 
                             </div> 
                             <div className = "profile-title" style = {{fontSize : "19px"}}>Coverletter</div>
                             <div className="form-group">
-                                <input type="file" id="coverletter" onChange={this.uploadcoverletter} style = {{display : "none"}}/>
-                                <button type="file" className="btn arteco-btn-save" id="position-resume-typeahead" onClick = {this.openCoverLetterDialog} style = {{width : "180px"}}>Upload Coverletter
-                                </button>&nbsp;&nbsp;{this.state.Coverletter}
+                                <input type="file" id="coverletter" onBlur={this.halffilled('coverletter')} onChange={this.uploadcoverletter} style = {{display : "none"}}/>
+                                <button type="file" className="btn arteco-btn-save" id="position-coverletter-typeahead" onClick = {this.openCoverletterDialog} style = {{width : "180px"}}>Upload Coverletter
+                                </button>&nbsp;&nbsp;{this.state.coverletter} 
                             </div> 
                         <div className = "job-application-consents ember-view">We include a copy of your full profile with your application
                         <br></br>
@@ -326,13 +360,14 @@ class JobApply extends Component{
     }
 }
 
-function validateprofile(firstname, lastname, phonenumber, email, resume) {
+function validateprofile(firstname, lastname, phonenumber, email, address, resume) {
     // true means invalid, so our conditions got reversed
     return {
       firstname: firstname.length === 0, 
       lastname: lastname.length === 0,
-      phonenumber: phonenumber.length === 0,
-      email: email.length === 0,
+      phonenumber: phonenumber.length < 10 || phonenumber.length > 10,
+      email: email.length === 0 || !validator.isEmail(email.value),
+      address : address.length === 0,
       resume: resume.length === 0,
     };
 }
