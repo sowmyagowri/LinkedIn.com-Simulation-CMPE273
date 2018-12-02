f08b45ca4bc65cec97eb9339951674f37c8d273f
Nirbhay Kekre <Nirbhay.Kekre@gmail.com>
2018-12-01 23:24:17 -0800

    updating caching on get job for recruiter

--- kafka_backend/services/getJobsByRecruiter.js
+++ kafka_backend/services/getJobsByRecruiter.js
@@ -1,6 +1,5 @@
 var { Jobs } = require('../models/job');
 const { prepareInternalServerError, prepareSuccess } = require('./responses');
-const {redisClient} = require('./../config/redisClient')
 
 async function handle_request(msg, callback) {
     console.log("In handle request:" + JSON.stringify(msg));
@@ -9,36 +8,13 @@ async function handle_request(msg, callback) {
     let resp = {};
     try {
         var recruiter_email = msg.recruiterEmail;
-        let redisKey = "jobsByRecruiter_" + recruiter_id;
-        redisClient.get(redisKey, async function (err, job_list) {
-            if (!err && job_list != null) {
-                console.log("Get job_list : job_list found in cache");
-                //console.log(job_list);
-                job_list = JSON.parse(job_list);
-            } else {
-                console.log("Get job_list : inserting job_list into cache");
-                //Main Logic for fetching jobs by recruiter
-                let job_list = await Jobs.find({ 'posted_by': recruiter_email }, { title: 1, company: 1, job_description: 1, industry: 1, employment_type: 1, location: 1, job_function: 1, company_logo: 1, posted_date: 1, expiry_date: 1 });
-                /**********/
-                if (job_list) {
-                    redisClient.set(redisKey, JSON.stringify(job_list), function (error, reply) {
-                        if (error) {
-                            console.log(error);
-                        }
-                        console.log(reply);
-                    });
-                    //cache will expire in 30 secs
-                    redisClient.expire(redisKey, 30);
-                }
-            }
-            resp = prepareSuccess({ "allJobs": job_list });
-            callback(null, resp);
-        });
+        let job_list = await Jobs.find({ 'posted_by': recruiter_email }, { title: 1, company: 1, job_description: 1, industry: 1, employment_type: 1, location: 1, job_function: 1, company_logo: 1, posted_date: 1, expiry_date: 1 });
+        resp = prepareSuccess({ "allJobs": job_list });
     } catch (err) {
         console.log("Error: ", err);
         resp = prepareInternalServerError();
-        callback(null, resp);
     }
+    callback(null, resp);
 }
 
 module.exports = { 
