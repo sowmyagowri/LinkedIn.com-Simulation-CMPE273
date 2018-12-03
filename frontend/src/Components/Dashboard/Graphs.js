c8fe7d2203a2943bce77b0c165f64e499c24559a
Akhilesh Anand <akhilesh.anand@sjsu.edu>
2018-12-02 23:54:55 -0800

    Update Graphs.js

--- frontend/src/Components/Dashboard/Graphs.js
+++ frontend/src/Components/Dashboard/Graphs.js
@@ -7,7 +7,8 @@ import axios from  "axios"
 import URI from "../../constants/URI"
 
 var BarChart = require("react-chartjs").Bar;
-
+var LineChart = require("react-chartjs").Line;
+var PieChart = require("react-chartjs").Pie;
 
 
 
@@ -18,7 +19,12 @@ class RecruiterGraphs extends Component {
       topten:null,
       clicksPerJob:null,
       topTenMonth:"1",
-      unpopularJobs:null
+      unpopularJobs:null,
+      jobs:[],
+      cityWiseMonth:"1",
+      cityWiseJobTitle:null,
+      cityWise:null
+
 
     };
 
@@ -30,7 +36,22 @@ class RecruiterGraphs extends Component {
   }
 
 
-    
+  CityWiseMonthChangeHandler= (e)=>{
+    this.setState({
+      cityWiseMonth:e.target.value+""
+    })
+    this.CityWiseGraph(this.state.cityWiseJobTitle, e.target.value+"")
+
+   }
+
+   CityWiseJobChangeHandler= (e)=>{
+    this.setState({
+      cityWiseJobTitle:e.target.value+""
+    })
+    this.CityWiseGraph( e.target.value+"", this.state.cityWiseMonth)
+  }
+
+
 
     TopTenMonthChangeHandler= (e)=>{
      this.setState({
@@ -130,19 +151,14 @@ class RecruiterGraphs extends Component {
         }
       }).then((res)=>{
         if (res.status === 200) {
-          console.log("Hello", res);
+          console.log(res.data.colors);
           data = {
             labels: res.data.label,
             datasets: [
               {
                 label: "My dataset",
-                fillColor: "rgba(151,187,205,0.2)",
-                strokeColor: "rgba(151,187,205,1)",
-                pointColor: "rgba(151,187,205,1)",
-                pointStrokeColor: "#fff",
-                pointHighlightFill: "#fff",
-                pointHighlightStroke: "rgba(151,187,205,1)",
-                data: res.data.values
+                fillColor:  res.data.colors,
+                data: [5,4,2,1,6]
               }
             ]
           };
@@ -157,10 +173,71 @@ class RecruiterGraphs extends Component {
     }
 
 
+
+    CityWiseGraph =(jobID, month)=>{
+      console.log(jobID, month);
+      let data = null;
+      axios.defaults.withCredentials = true;
+      axios.defaults.headers.common["Authorization"] =localStorage.getItem("user");
+      axios.get(`${URI.ROOT_URL}/graph_citywise_applications`, {
+        params: {
+          jobID,
+          month
+        }
+      }).then((res)=>{
+        if (res.status === 200) {
+          console.log(res);
+        //   data = {
+        //     labels: res.data.label,
+        //     datasets: [
+        //       {
+        //         label: "My dataset",
+        //         fillColor:  res.data.colors,
+        //         data: [5,4,2,1,6]
+        //       }
+        //     ]
+        //   };
+        //   console.log("City Wise Jobs",data)
+        //  this.setState({
+        //   cityWise:data
+        //  })
+        }
+      }).catch((err)=>{
+          console.log(err);
+      })
+    }
+
+
+
+
+    populateCity =()=>{
+      let recruiterEmail = "recruiter4@gmail.com";
+      axios.defaults.withCredentials = true;
+      axios.defaults.headers.common["Authorization"] =localStorage.getItem("user");
+      axios.get(`${URI.ROOT_URL}/get_jobs_by_recruiter`, {
+        params: {
+          recruiterEmail,
+        }
+      }).then((res)=>{
+        if (res.status === 200) {
+          this.setState({
+            jobs:res.data.allJobs,
+            cityWiseJobTitle:res.data.allJobs[0]._id
+          })
+          this.CityWiseGraph(res.data.allJobs[0]._id, "1")
+        }
+      }).catch((err)=>{
+          console.log(err);
+      })
+     
+    }
+
+
    componentDidMount() {
       this.TopTenJobs(this.state.topTenMonth)
       this.ClicksPerJob()
       this.UnpopularJobs();
+    this.populateCity();
   }
 
   componentWillMount(){
@@ -169,6 +246,12 @@ class RecruiterGraphs extends Component {
   
   }
   render() {
+   let  cities = this.state.jobs.map(job => {
+      return (
+        <option value={job._id}>{job.title}</option>
+      );
+    });
+
 
     return (
       <div>
@@ -179,8 +262,9 @@ class RecruiterGraphs extends Component {
 
 
         <div className="container text-center">
-        <h3>Statistical Analysis</h3>
-
+        <h1 style={{fontWeight:"200"}}>Statistical Analysis</h1>
+        <h2 style={{fontWeight:"200"}}>Graphs</h2>
+        <br/><br/>
           <div className="row">
             <div className="col-12">
               <div className="card shadow-lg">
@@ -188,7 +272,7 @@ class RecruiterGraphs extends Component {
                   <h5 className="card-title">Top 10 Jobs Per Month</h5>
                   <div className="row">
                     <div className="col-8">
-                    {this.state.topten===null ? null: <BarChart data={this.state.topten} width="600" height="250" /> }
+                    {this.state.topten===null ? null: <LineChart data={this.state.topten} width="600" height="250" /> }
                     </div>
                     <div className="col-4">
                       <br />
@@ -255,7 +339,53 @@ class RecruiterGraphs extends Component {
           </div> 
 
 
-          
+
+          <br />
+          <br />
+
+          <div className="row">
+            <div className="col-12">
+              <div className="card shadow-lg">
+                <div className="card-body">
+                  <h5 className="card-title">City Wise Jobs per Month</h5>
+                  <div className="row">
+                    <div className="col-8">
+                    {this.state.cityWise===null ? null: <LineChart data={this.state.cityWise} width="600" height="250" /> }
+                    </div>
+                    <div className="col-4">
+                      <br />
+                      <br />
+                      <h5> Select Job Title</h5>
+                      <select onChange={this.CityWiseJobChangeHandler}  className="form-control">
+                      {cities}  
+                      </select>
+                      <br/> 
+                      <h5> Select Month</h5>
+                      <select onChange={this.CityWiseMonthChangeHandler}  className="form-control">
+                        <option value="1">January</option>
+                        <option value="2">Febuary</option>
+                        <option value="3">March</option>
+                        <option value="4">April</option>
+                        <option value="5">May</option>
+                        <option value="6">June</option>
+                        <option value="7">July</option>
+                        <option value="8">August</option>
+                        <option value="9">September</option>
+                        <option value="10">October</option>
+                        <option value="11">November</option>
+                        <option value="12">December</option>
+                      </select>
+
+
+                      
+                      
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
         </div>
       </div>
     );
